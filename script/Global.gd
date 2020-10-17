e24bde1f358471bae51735564cb7808fd78b16d9
lpenaud <l.penaud@zaclys.net>
2020-10-17 19:08:22 +0200

    Revert "Add loading screen"
    Revert not stable commit e43e2c0ccaa17ddf19ad168c50d3b5b90e03ca7a.

--- script/Global.gd
+++ script/Global.gd
@@ -1,25 +1,27 @@
 extends Node
-# See: https://docs.godotengine.org/en/3.1/getting_started/step_by_step/singletons_autoload.html#custom-scene-switcher
- 
-var current_scene: Node
 
+var current_scene = null
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() - 1)
 
-func goto_scene(path: String):
+func goto_scene(path):
 	call_deferred("_deferred_goto_scene", path)
 
-func goto_scene_from_resource(scene_resource: Resource):
+func _deferred_goto_scene(path):
 	# It is now safe to remove the current scene
 	current_scene.free()
-	current_scene = scene_resource.instance()
+
+	# Load the new scene.
+	var s = ResourceLoader.load(path)
+
+	# Instance the new scene.
+	current_scene = s.instance()
 
 	# Add it to the active scene, as child of root.
 	get_tree().get_root().add_child(current_scene)
 
 	# Optionally, to make it compatible with the SceneTree.change_scene() API.
 	get_tree().set_current_scene(current_scene)
-
-func _deferred_goto_scene(path: String):
-	goto_scene_from_resource(ResourceLoader.load(path))
--- script/MenuTitle.gd
+++ script/MenuTitle.gd
@@ -5,4 +5,4 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_ButtonNewGame_pressed():
-	$LoadingControl.load_scene("res://root.tscn")
+	Global.goto_scene("res://root.tscn")
